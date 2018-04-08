3866ee3a88d9892d6a115f1e52fb39f804702741
viseth89 <viseth89@gmail.com>
2018-04-08 08:09:56 -0500

    working on 9 and understanding 8

---
+++ Exercise_8.4(Adam).py
@@ -0,0 +1,10 @@
+with open('romeoandjuliet.txt') as file:
+	lines = file.readlines()
+
+words = []
+for line in lines:
+	split_line = line.split()
+	words.extend(split_line)
+
+words.sort()
+print(words)
